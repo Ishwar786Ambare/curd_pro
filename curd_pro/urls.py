e93474740060854965291fda6959740a2aea962e
ishwar ambare <ishuambare@gmail.com>
2020-05-29 09:43:12 +0530

    commit-2

--- curd_pro/urls.py
+++ curd_pro/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from curdapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.test),
 ]
